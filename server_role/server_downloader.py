aadd7fe26f294d93ef09ad8d2122585eea98c8c3
方利 <fangli@autohome.com.cn>
2016-11-11 18:19:13 +0800

    藏宝阁用户信息爬取

--- server_role/server_downloader.py
+++ server_role/server_downloader.py
@@ -9,12 +9,16 @@ class HtmlDownloader(object):
     def download(self, url):
         if url is None:
             return None
-
+        cookies = None
         headers = {'Content-Type': 'text/plain;charset=UTF-8',
                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 '
                                  '(KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-                   "Referer": "http://tx3.cbg.163.com/cgi-bin/equipquery.py?act=show_overall_search"}
-        response = requests.get(url, headers=headers)
+                   'Referer': 'http://tx3.cbg.163.com/cgi-bin/equipquery.py?act=show_overall_search',
+                   'Origin': 'http://tx3.cbg.163.com'}
+
+        response = requests.get(url, headers=headers, cookies=cookies)
+        cookies = response.cookies
         if response.status_code != 200:
             return None
+        print(response.cookies)
         return response.text
