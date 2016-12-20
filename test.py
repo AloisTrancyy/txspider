cb8026c3a0f744d8ac559df0a76cfc88141d3961
fangli <fangli@autohome.com.cn>
2016-12-20 15:53:47 +0800

    优化爬取策略

--- test.py
+++ test.py
@@ -8,16 +8,11 @@ import traceback
 import datetime
 import time
 import json
+import re
 
-config = configparser.ConfigParser()
-config.read("config.ini")
-dbconfig = {
-    'host': config.get('mysql', 'host'),
-    'port': config.getint('mysql', 'port'),
-    'user': config.get('mysql', 'user'),
-    'password': config.get('mysql', 'password'),
-    'db': config.get('mysql', 'db'),
-    'charset': config.get('mysql', 'charset'),
-    'cursorclass': pymysql.cursors.DictCursor
-}
-print(type(time.strftime('%Y-%m-%d',time.localtime(time.time()))))
+fresh_time = '12月20日 10:23'
+mats = re.findall(r"\d+\.?\d*", fresh_time)
+print(mats)
+
+new_time = datetime.datetime(2016, int(mats[0]), int(mats[1]), int(mats[2]), int(mats[3]))
+print(new_time)
