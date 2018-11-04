0b4ac9e3010deb839516d5095020b7e17b185d28
方利 <fangli@autohome.com.cn>
2018-11-04 09:39:12 +0800

    爬虫改成是怦s'c'fa'oj

--- CbgSpider/spiders/cbg.py
+++ CbgSpider/spiders/cbg.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
+import time
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
-from urllib.parse import urljoin
 
 
 class CbgSpider(scrapy.Spider):
@@ -13,29 +13,30 @@ class CbgSpider(scrapy.Spider):
         'http://tx3-ios2.cbg.163.com/cbg-center/search.py?num_per_page=15&products=tx2&act=basic_search_role&order_refer=8&price_min=50000&price_max=29999900&level_min=69&level_max=69&school=1&page=1']
 
     def parse(self, response):
-        base_url = 'http://tx3-ios2.cbg.163.com/cbg-center/search.py?num_per_page=15&products=tx2&act=basic_search_role&order_refer=8&price_min=50000&price_max=29999900&level_min=69&level_max=69&school=1&page=1'
+        time.sleep(2)
+        base_url = 'http://tx3-ios2.cbg.163.com/cbg-center/search.py?num_per_page=15&products=tx2&act=basic_search_role&order_refer=8&price_min=50000&price_max=29999900&level_min=69&level_max=69'
 
         old_url = response.url
         url_parsed = urlparse(old_url)
-        page = parse_qs(url_parsed.query)["page"]
-        school = parse_qs(url_parsed.query)["school"]
-
+        page = parse_qs(url_parsed.query)["page"][0]
+        school = parse_qs(url_parsed.query)["school"][0]
         result = json.loads(response.body)  # 将Json格式数据处理为字典类型
         items = result.get("equip_list")
-        if len(items) == 0:
-            next_url = urljoin(base_url, "&page=" + str(int(page) + 1))
-            next_url = urljoin(next_url, "&school=" + str(int(school) + 1))
-            print(next_url)
-            yield scrapy.FormRequest(
-                url=next_url,
-                # formdata={"modelCode": item['seriesId'], "agent": "7"},
-                callback=self.parse,
-                dont_filter=True
-            )
+        if items is None or len(items) == 0:
+            if school == 11:
+                return
+            else:
+                next_url = base_url + "&page=1&school=" + str(int(school) + 1)
+                print(next_url)
+                yield scrapy.FormRequest(
+                    url=next_url,
+                    callback=self.parse,
+                    dont_filter=True
+                )
         else:
             for item in items:
-                next_url = urljoin(old_url, "&school=" + str(int(school) + 1))
-                print(next_url)
+                next_url = base_url + "&school=" + school + "&page=" + str(int(page) + 1)
+                print(next_url, item)
                 yield scrapy.FormRequest(
                     url=next_url,
                     callback=self.parse,
